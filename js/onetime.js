03e2fee7c064d3aee38ecbc671de3a5382b385b0
Taha Hadian <tahamamad49@gmail.com>
2025-07-21 14:05:58 +0330

    fix faq

--- js/onetime.js
+++ js/onetime.js
@@ -30,18 +30,18 @@ class SocialRight extends HTMLElement {
   connectedCallback() {
     this.innerHTML = `
     <div class="hidden lg:flex fixed top-1/3 right-4 z-10 flex-col space-y-1 pointer-events-auto">
-        <a href="https://t.me/TAWSS" class="bg-red-900 hover:bg-red-800 px-4 py-3 rounded-t-xl border-2 border-white/50">
-        <img src="/Images/telegram.png" alt="telegram icon" loading="lazy">
-        </a>
-        <a href="https://steamcommunity.com/id/TahazuS" class="bg-red-900 hover:bg-red-800 px-4 py-3 rounded-sm border-2 border-white/50">
-        <img src="/Images/steam.png" alt="steam icon" loading="lazy">
-        </a>
-        <a href="https://x.com/TahaHadian" class="bg-red-900 hover:bg-red-800 px-4 py-3 rounded-sm border-2 border-white/50">
-        <img src="/Images/twitter.png" alt="x.com icon" loading="lazy">
-        </a>
-        <a href="https://discord.gg/a7PrkNjXWk" class="bg-red-900 hover:bg-red-800 px-4 py-3 rounded-b-xl border-2 border-white/50">
-        <img src="/Images/discord.png" alt="discord icon" loading="lazy">
-        </a>
+      <a href="https://t.me/TAWSS" class="bg-red-900 hover:bg-red-800 px-4 py-3 rounded-t-xl border-2 border-white/50">
+        <img src="/Images/telegram.png" alt="آیکن تلگرام" loading="lazy">
+      </a>
+      <a href="https://steamcommunity.com/id/TahazuS" class="bg-red-900 hover:bg-red-800 px-4 py-3 border-2 border-white/50">
+        <img src="/Images/steam.png" alt="آیکن استیم" loading="lazy">
+      </a>
+      <a href="https://x.com/TahaHadian" class="bg-red-900 hover:bg-red-800 px-4 py-3 border-2 border-white/50">
+        <img src="/Images/twitter.png" alt="آیکن ایکس (توییتر)" loading="lazy">
+      </a>
+      <a href="https://discord.gg/a7PrkNjXWk" class="bg-red-900 hover:bg-red-800 px-4 py-3 rounded-b-xl border-2 border-white/50">
+        <img src="/Images/discord.png" alt="آیکن دیسکورد" loading="lazy">
+      </a>
     </div>
     `;
   }
@@ -54,10 +54,10 @@ class HeaderName extends HTMLElement {
   connectedCallback() {
     this.innerHTML = `
     <header class="text-center py-8 m-auto">
-        <a href="/" class="relative inline-block pointer-events-auto">
-          <span class="absolute -top-5 right-12 md:-top-5 md:right-16 bg-amber-300 text-stone-900 text-xs px-2 py-0.5 rounded-full">بتا</span>
-          <h1 class="text-4xl md:text-5xl font-bold">تـــاز</h1>
-        </a>
+      <a href="/" class="relative inline-block pointer-events-auto">
+        <span class="absolute -top-5 right-12 md:-top-5 md:right-16 bg-amber-300 text-stone-900 text-xs px-2 py-0.5 rounded-full">بتا</span>
+        <h1 class="text-4xl md:text-5xl font-bold">تـــاز</h1>
+      </a>
     </header>
     `;
   }
@@ -69,12 +69,13 @@ customElements.define('header-name', HeaderName);
 class AlertHeader extends HTMLElement {
   connectedCallback() {
     this.innerHTML = `
-    <div class="flex justify-center pointer-events-auto text-l mx-4">
-        <a href="https://t.me/TahazuS" class="flex items-center bg-amber-300 text-black hover:bg-amber-400 px-4 py-3 rounded-md border-2 border-white/50">
-        <svg xmlns="http://www.w3.org/2000/svg" class="w-4 h-4 ml-2 inline" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+    <div class="flex justify-center mx-4 pointer-events-auto">
+      <a href="https://t.me/TahazuS" class="flex items-center bg-amber-300 text-black hover:bg-amber-400 px-4 py-3 rounded-md border-2 border-white/50">
+        <svg xmlns="http://www.w3.org/2000/svg" class="w-4 h-4 ml-2" fill="none" viewBox="0 0 24 24" stroke="currentColor">
           <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13 16h-1v-4h-1m0-4h.01M12 2a10 10 0 100 20 10 10 0 000-20z" />
         </svg>
-        تمام خریدها از در تلگرام انجام می‌شود برای هر گونه سوال یا استعلام قیمت، به tahazus@ پیام دهید.</a>
+        تمام خریدها از طریق تلگرام انجام می‌شود. برای سوال یا استعلام قیمت، به @TahazuS پیام دهید.
+      </a>
     </div>
     `;
   }
